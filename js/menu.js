b4b7058cfce0c2d1288fddf78b3623758a1c0f37
TimeBookpage <98508328+TimeBookpage@users.noreply.github.com>
2022-04-13 07:15:05 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
-document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
-document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
-document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
-document.writeln("<li style=\"--i: 4\"><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
-document.writeln("<li style=\"--i: 5\"><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
-document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
-document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> RavelloH.<br> All rights reserved.</b>");
+document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> 主页</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/me/works/\'><span class=\'iconfontsmall icon-app\'></span> 产品</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/me/articles\'><span class=\'iconfontsmall icon-article\'></span> 文章</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/me/tag/\'><span class=\'iconfontsmall icon-tag\'></span> 标签</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/me/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
+document.writeln("<li style=\"--i: 5\"><a href=\'/me/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
+document.writeln("<li style=\"--i: 6\"><a href=\'/me/about/\'><span class=\'iconfontsmall icon-about\'></span> 关于</a></li>");
+document.writeln("<b><br><br>Copyright © 2021 - <b id=\'year\'></b> 庞熙睿.<br> All rights reserved.</b>");
