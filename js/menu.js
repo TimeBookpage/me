738b6d03fe0fd3be866618ff6d733964f74995e7
TimeBookpage <98508328+TimeBookpage@users.noreply.github.com>
2022-04-13 07:17:46 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall
 document.writeln("<li style=\"--i: 1\"><a href=\'/me/works/\'><span class=\'iconfontsmall icon-app\'></span> 产品</a></li>");
 document.writeln("<li style=\"--i: 2\"><a href=\'/me/articles\'><span class=\'iconfontsmall icon-article\'></span> 文章</a></li>");
 document.writeln("<li style=\"--i: 3\"><a href=\'/me/tag/\'><span class=\'iconfontsmall icon-tag\'></span> 标签</a></li>");
-document.writeln("<li style=\"--i: 4\"><a href=\'/me/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
-document.writeln("<li style=\"--i: 5\"><a href=\'/me/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/me/classification/\'><span class=\'iconfontsmall icon-classification\'></span> 分类</a>");
+document.writeln("<li style=\"--i: 5\"><a href=\'/me/friends/\'><span class=\'iconfontsmall icon-friend\'></span> 友情链接</a></li>");
 document.writeln("<li style=\"--i: 6\"><a href=\'/me/about/\'><span class=\'iconfontsmall icon-about\'></span> 关于</a></li>");
 document.writeln("<b><br><br>Copyright © 2021 - <b id=\'year\'></b> 庞熙睿.<br> All rights reserved.</b>");
